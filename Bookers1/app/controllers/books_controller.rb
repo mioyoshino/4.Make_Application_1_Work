55e0095536b7474d0b20763834d84bbf4ced40a2
mioyoshino <mio.y@infratop.jp>
2020-02-29 05:40:09 +0000

    make controller

---
+++ Bookers1/app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
--- Bookers1/config/routes.rb
+++ Bookers1/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/top'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'top/index'
+  get 'top/show'
+  get 'top/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
