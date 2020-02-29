679092482aacf7654a183d459d2729a6f7bf6f7b
mioyoshino <mio.y@infratop.jp>
2020-02-29 05:59:41 +0000

    make routing

--- Bookers1/config/routes.rb
+++ Bookers1/config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'top/index'
-  get 'top/show'
-  get 'top/edit'
+  resources :books
+  
+  root :to => 'books#top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
