e999cbbb569bcb69ba9d973243be71698ee98dd1
mioyoshino <mio.y@infratop.jp>
2020-02-29 10:51:08 +0000

    make flash message

--- Bookers1/app/controllers/books_controller.rb
+++ Bookers1/app/controllers/books_controller.rb
@@ -23,17 +23,27 @@ class BooksController < ApplicationController
   def create
     # ストロングパラメータを設定
     book = Book.new(book_params)
+
+    # 投稿成功後のサクセスメッセージを設定
     # DBへ保存する
-    book.save
-    # 投稿が完了したらindexへリダイレクトさせる
-    redirect_to books_path
+    if book.save
+      redirect_to books_path, notice: "successfully"
+    else
+      render action: :create
+    end
+    
   end
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-
-    redirect_to book_path(book.id)
+    
+    # 更新成功後のサクセスメッセージを設定
+    if book.update(book_params)
+       redirect_to book_path(book), notice: "successfully"
+     else
+       flash.now[:alert] = 'failed'
+       render edit_book_path(book)
+     end
 
   end
 
